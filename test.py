fa60a0b3987d1d93cc9b8946c4931e8d3c3e7f98
Michael <Misha2006060906@gmail.com>
2024-11-30 23:52:20 +0300

    Update test.py

--- test.py
+++ test.py
@@ -4,14 +4,14 @@ import library
 if __name__ == "__main__":
     test_library = library.BookLibrary()
     try:
-        print(test_library.add_book("Ночь в Лиссабоне", "Эрих Мария Ремарк", "1962"))
-        print(test_library.add_book("На Западном фронте без перемен", "Эрих Мария Ремарк", "1928"))
+        test_library.add_book("Ночь в Лиссабоне", "Эрих Мария Ремарк", "1962")
+        test_library.add_book("На Западном фронте без перемен", "Эрих Мария Ремарк", "1928")
 
         print(test_library.get_books(author="Эрих Мария Ремарк"))
 
-        print(test_library.add_book("Заводной апельсин", "Энтони Бёрджесс", "1962"))
-        print(test_library.add_book("Тестовая книга", "Энтони Бёрджесс", "1962"))
-        print(test_library.add_book(title="%^", author="12-", year="qweewr--=qwe")) # пытаюсь создать книгу с неправильными параметрами
+        test_library.add_book("Заводной апельсин", "Энтони Бёрджесс", "1962")
+        test_library.add_book("Тестовая книга", "Энтони Бёрджесс", "1962")
+        test_library.add_book(title="%^", author="12-", year="qweewr--=qwe") # пытаюсь создать книгу с неправильными параметрами
 
         print(test_library.get_books(author="Энтони Бёрджесс", year="1962"))
         print(test_library.get_all_books())
