b6dc7448285f27994ce7e85aad5c79277cc414d9
Michael <Misha2006060906@gmail.com>
2024-12-05 10:32:52 +0300

    Update library.py

--- library.py
+++ library.py
@@ -1,143 +1,135 @@
 import json
-from pprint import pprint
-import library
-
-if __name__ == "__main__":
-
-    print("""Программа для создания, управления и редактирования собственной библиотеки. 
-    Для начала работы введите /start""")
+from string import punctuation, digits
+from dataclasses import dataclass, asdict
 
 
-    def write_to_file(lib_name: str) -> None:
-        """Функция для записи библиотеки в файл"""
+class _LibraryError:
+    """Класс для обработки исключений в библиотеке"""
+
+    title_error = "В параметр title введены неверные значения"
+    author_error = "В параметр author введены неверные значения"
+    year_error = "В параметр year введены неверные значения"
+    id_error = "Книги с введённыи id нет в библиотеке"
+    status_error = "Введён неверный статус"
+
+
+@dataclass
+class _Book:
+    """Класс для инициализации книги"""
+    _title: str
+    _author: str
+    _year: str
+    _status: str = "в наличии"
+
+
+class BookLibrary:
+    """Класс для создания библиотеки"""
+
+    __if_add = "Книга успешно добавлена"
+    __if_delete = "Книга успешно удалена"
+    __if_new_status = "Статус успешно обновлён"
+    __if_old_status = "Введённый статус уже установлен"
+
+    def __init__(self, book_id: int = 0):
+        self.Library = {
+            "books": {}
+        }
+        self.__id = book_id
+
+    def add_book(self, title: str, author: str, year: str) -> None:
+        """
+        Функция для добавления книги в библиотеку
+
+        Параметры:
+        1. title - Название книги
+        2. author - Автор книги
+        3. year - Год выпуска книги
+        4. status = Статус книги, принимает 2 значения (в наличии / выдана)
+        """
+
+        title_chars = "".join(punctuation.split("-"))
+        author_chars = "".join(punctuation.split("-")) + digits
+        param_errors = 0  # переменная для подсчёта ошибка в параметрах
+
+        # ошибка, если в названии книги есть знак пунктуации помимо дефиса
+        if any([char in title_chars for char in title]) or not title:
+            print(_LibraryError.title_error)
+            param_errors += 1
+        # ошибка, если в году издания книги хотя бы один знак - это цифра/знак пунктуации помимо тире
+        if any([char in author_chars for char in author]) or not author:
+            print(_LibraryError.author_error)
+            param_errors += 1
+        # ошибка, если в году издания книги хотя бы один знак не цифра
+        if not year.isdigit():
+            print(_LibraryError.year_error)
+            param_errors += 1
+        if param_errors > 0:
+            return None
+
+        book = _Book(title, author, year)
+        self.__id += 1
+        self.Library["books"][self.__id] = asdict(book)
+        print(self.__if_add)
+
+    def delete_book(self, book_id: int) -> None:
+        """
+        Функция для удаления книги из библиотеки
+
+        Параметры:
+        1. book_id - id книги
+        """
+
         try:
-            with open(f"{lib_name}", "w") as lib_file:
-                lib_file.write(json.dumps(My_Library.Library, ensure_ascii=False))
-                print("Бибилиотека успешно создана")
-        except OSError:
-            print("Ошибка: недопустимое имя файла")
+            del self.Library["books"][book_id]
+        except KeyError:
+            print(_LibraryError.id_error)
+        else:
+            print(self.__if_delete)
 
+    def get_books(self, title: str = "", author: str = "", year: str = "") -> str:
+        """
+        Функция для отображения книги по id
 
-    start = input()
+        Параметры:
+        1. title - Название книги
+        2. author - Автор книги
+        3. year - Год выпуска книги
+        """
 
-    if start == "/start":
-        # булевая переменная для проверки того, выводилась ли подсказка о команде /help
-        is_message_about_help = False
-        # словарь для хранения библиотек
+        required_books = {}
 
-        Libraries = {}
+        for index, book_param in self.Library["books"].items():
+            if (((not title) or book_param["_title"] == title) and
+                    ((not author) or book_param["_author"] == author) and
+                    ((not year) or book_param["_year"] == year)):
+                required_books[index] = self.Library["books"][index]
 
-        while True:
-            if not is_message_about_help:
-                print("""Для того, чтобы узнать список команд - введите /help""")
-            # после вывода подсказки переменная принимает значение  True и не выводит подсказку
-            is_message_about_help = True
+        return json.dumps(required_books, ensure_ascii=False)
 
-            message = input()
+    def get_all_books(self) -> str:
+        """
+        Функция для отображения всех книг
+        """
 
-            if message == "/help":
-                print("""  Команды:
-        1. /create_lib - Команда для создания бибилиотеки
-        2. /add_book - Добавляет книгу с введёнными 
-        пользователем title/author/year
-        3. /get_all_books - Отображает библиотеку
-        4. /get_books - Отображает книги по title/author/year
-        5. /delete_book - Удаляет книгу по id
-        6. /set_new_status - Обновляет статус книги.
-        Принимает только 2 статуса: 'в наличии'/'выдана'
-        7. /get_libs - Отображает все библиотеки
-        8. /end - Завершает работу приложения""")
+        return json.dumps(self.Library["books"], ensure_ascii=False)
 
-            elif message == "/create_lib":
-                LibraryName = input("Введите имя библиотеки: ")
-                My_Library = library.BookLibrary()
-                Libraries[LibraryName] = My_Library
+    def set_new_status(self, book_id: int, status: str) -> None:
+        """
+        Функция для изменения статуса книги по id
 
-                write_to_file(LibraryName)
+        Параметры:
+        1. book_id - id книги
+        """
 
-            elif message == "/add_book":
-                LibraryName = input("""Введите имя библиотеки, в которую вы хотите добавить книгу: """)
+        match status:
+            case "выдана" | "в наличии":
                 try:
-                    Libraries[LibraryName]
-                except KeyError:
-                    print("Ошибка: бибилиотеки с таким именем не существует")
-                else:
-                    lib = Libraries[LibraryName]
-                    title = input("Введите название книги: ").strip()
-                    author = input("Введите автора книги: ").strip()
-                    year = input("Введите год написания книги: ").strip()
-
-                    lib.add_book(title=title, author=author, year=year)
-                    write_to_file(LibraryName)
-
-            elif message == "/get_all_books":
-                LibraryName = input("""Введите имя библиотеки: """)
-                try:
-                    lib = Libraries[LibraryName]
-                    pprint(lib.get_all_books())
-                except KeyError:
-                    print("Ошибка: бибилиотеки с таким именем не существует")
-
-            elif message == "/get_books":
-                LibraryName = input("""Введите имя библиотеки: """)
-                try:
-                    Libraries[LibraryName]
-                except KeyError:
-                    print("Ошибка: бибилиотеки с таким именем не существует")
-                else:
-                    lib = Libraries[LibraryName]
-                    title = input("Введите название книги: ")
-                    author = input("Введите автора книги: ")
-                    year = input("Введите год написания книги: ")
-
-                    pprint(lib.get_books(title=title, author=author, year=year))
-
-            elif message == "/delete_book":
-                LibraryName = input("""Введите имя библиотеки: """)
-                try:
-                    Libraries[LibraryName]
-                except KeyError:
-                    print("Ошибка: бибилиотеки с таким именем не существует")
-                else:
-                    try:
-                        book_id = int(input("Введите id книги: "))
-                    except ValueError:
-                        print("id должно быть числом")
+                    if self.Library["books"][book_id]["_status"] != status:
+                        self.Library["books"][book_id]["_status"] = status
+                        print(self.__if_new_status)
                     else:
-                        lib = Libraries[LibraryName]
-                        lib.delete_book(book_id=book_id)
-
-                        write_to_file(LibraryName)
-
-            elif message == "/set_new_status":
-                LibraryName = input("""Введите имя библиотеки: """)
-                try:
-                    Libraries[LibraryName]
+                        print(self.__if_old_status)
                 except KeyError:
-                    print("Ошибка: бибилиотеки с таким именем не существует")
-                else:
-                    try:
-                        book_id = int(input("Введите id книги: "))
-                    except ValueError:
-                        print("id должно быть числом")
-                    else:
-                        lib = Libraries[LibraryName]
-                        new_status = input("Введите новый статус книги: ")
-                        lib.set_new_status(book_id=book_id, status=new_status)
-
-                        write_to_file(LibraryName)
-
-            elif message == "/get_libs":
-                pprint(Libraries)
-
-            elif message == "/end":
-                break
-
-            elif message == "":
-                continue
-
-            else:
-                print("Ошибка: неизвестная команда")
-
-    else:
-        print("Ошибка: неизвестная команда")
+                    print(_LibraryError.id_error)
+            case _:
+                print(_LibraryError.status_error)
